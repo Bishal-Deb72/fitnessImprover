e226943e6f73774d17fdfeae7db9adad02e614af
Bishal-Deb72 <137647034+Bishal-Deb72@users.noreply.github.com>
2024-12-14 19:19:17 +0530

    Add files via upload

---
+++ src/pages/Services.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Layout } from '../component/layouts/layouts'
+
+export const Services = () => {
+  return (
+    <Layout>
+      <div>Services</div>
+    </Layout>
+  )
+}
